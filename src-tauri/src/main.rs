7dd01f7a9de37912fd19e1cfb4395320df007625
aramisjasso <aramisjasso@gmail.com>
2025-10-30 14:41:55 -0700

    chore: configure rust and sqlite database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// 1. Importa tu módulo de comandos
+mod commands;
+
 fn main() {
-    chulada_pos_lib::run()
+    tauri::Builder::default()
+        // 2. Registra tu comando
+        .invoke_handler(tauri::generate_handler![
+            commands::database::init_database
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
+import { invoke } from '@tauri-apps/api/core' 
 
 function App() {
+  //La App llama a rust una vez ha terminado de cargar. 
+  useEffect(() => {
+    invoke('init_database')
+      .then(() => console.log('[TAURI] Database initialized successfully'))
+      .catch((err:any) => console.error('[TAURI] Error initializing database:', err))
+  }, [])
+  
   return (
     <div className="container p-4">
       <h1 className="text-3xl font-bold text-primary-500">
