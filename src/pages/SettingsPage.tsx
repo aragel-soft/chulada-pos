b49ab9a2301cef335158dc931691eaba4d68920d
aramisjasso <aramisjasso@gmail.com>
2025-12-03 18:55:05 -0700

    tyle(settings): add top margin from settings title

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -36,7 +36,7 @@ export default function SettingsPage() {
   return (
     <div className="flex flex-col h-full p-4 gap-1">
       <div className="flex-none flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Configuración</h1>
+        <h1 className="text-3xl font-bold mt-2">Configuración</h1>
       </div>
 
       <div className="flex-none w-full">
