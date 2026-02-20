5693e4885bd36163a674de9fd5abf0b78a7f3cf1
aramisjasso <aramisjasso@gmail.com>
2026-02-20 13:57:59 -0700

    fix(reports): hide components in print

--- src/components/ui/data-table/data-table-column-header.tsx
+++ src/components/ui/data-table/data-table-column-header.tsx
@@ -29,11 +29,11 @@ export function DataTableColumnHeader<TData, TValue>({
       >
         <span>{title}</span>
         {column.getIsSorted() === "desc" ? (
-          <ArrowDown className="ml-2 h-4 w-4" />
+          <ArrowDown className="ml-2 h-4 w-4 print:hidden" />
         ) : column.getIsSorted() === "asc" ? (
-          <ArrowUp className="ml-2 h-4 w-4" />
+          <ArrowUp className="ml-2 h-4 w-4 print:hidden" />
         ) : (
-          <ArrowUpDown className="ml-2 h-4 w-4" />
+          <ArrowUpDown className="ml-2 h-4 w-4 print:hidden" />
         )}
       </Button>
     </div>
--- src/components/ui/data-table/data-table.tsx
+++ src/components/ui/data-table/data-table.tsx
@@ -157,7 +157,7 @@ export function DataTable<TData, TValue>({
     }
 
     return (
-      <div className="flex flex-col sm:flex-row gap-4 w-full">
+      <div className="flex flex-col sm:flex-row gap-4 w-full print:hidden">
         <div className="relative flex-1 min-w-[300px]">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
           <DebouncedInput
